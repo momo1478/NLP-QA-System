e52262d327a23b9eccea392b17f90a5c07737b7c
monishg <monishg@lab2-21.eng.utah.edu>
2018-11-16 14:13:01 -0700

    qa.sh pushed

---
+++ qa.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+#source ./env/bin/activate
+#python qa.py $1
+
+pip install --user virtualenv
+mkdir env
+python -m virtualenv env
+source ./env/bin/activate
+pip install spacy
+python -m spacy download en
+python qa.py $1
